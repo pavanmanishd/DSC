eabfb76a42a6cc7651993e28d9730fecbdc4f72b
Shubh Chudasama <65857597+c-shubh@users.noreply.github.com>
2022-07-03 13:17:03 +0530

    Resolve a bug and format code
    In the push function, pre increment tos value since tos starts from -1

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -6,7 +6,7 @@ char stack[size];
 int tos = -1;
 void push(char op)
 {
-	stack[tos++] = op;
+	stack[++tos] = op;
 }
 int pop()
 {
@@ -20,13 +20,13 @@ bool isOperator(char token)
 {
 	switch(token)
 	{
-		case '+': return true;
-		case '-': return true;
-		case '*': return true;
-		case '/': return true;
+		case '+':
+		case '-':
+		case '*':
+		case '/':
 		case '%': return true;
-		default : return false;
 	}
+	return false;
 }
 int getPriority(char op)
 {
@@ -37,45 +37,35 @@ int getPriority(char op)
 	else if(op == '(')
 		return 0;
 }
-void InfixToPostfix(char infix[],char postfix[])
+void infixToPostfix(char infix[], char postfix[])
 {
-	int i=0,j=0;
-	char token,x;
-	for(i=0;'\0'!= infix[i];i++)
+	int i=0, j=0;
+	char token, x;
+	for(i=0; '\0'!= infix[i]; i++)
 	{
 		token = infix[i];
-		if('('==token)
-		{
+		if('(' == token)
 			push(token);
-		}
-		else if(')'==token)
+		else if(')' == token)
 		{
 			//send all elements upto ( to postfix
 			while((tos>-1) && ('('!=(x=pop())))
-			{
 				postfix[j++] = x;
-			}
 		}
 		else if(true == isOperator(token))
 		{
 			//send all operator of equal or higher priority to postfix
 			while((tos>-1) && (getPriority(peep())>=getPriority(token)))
-			{
 				postfix[j++] = pop();
-			}
 			push(token);
 		}
 		else
-		{
 			postfix[j++] = token;
-		}
     }
 		//after all operators and operands are processed
 		//pop all operators from stack to postfix
 		while(tos>-1)
-		{
 			postfix[j++] = pop();
-		}
 		//make postfix a string
 		postfix[j] = '\0';
 }
@@ -84,8 +74,8 @@ int main(void)
 	char infix[81];
 	char postfix[81];
 	printf("Enter the infix expression : ");
-	scanf("%s",infix);
-	InfixToPostfix(infix,postfix);
-	printf("Postfix is : %s ",postfix);
+	scanf("%s", infix);
+	infixToPostfix(infix, postfix);
+	printf("Postfix is : %s\n", postfix);
 	return 0;
 }
