2e753f457f6d20bbee076cb33bb8e80494fb0ba8
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-07-18 22:32:51 +0530

    Update Selection_Sort.c

--- Selection_Sort.c
+++ Selection_Sort.c
@@ -43,7 +43,10 @@ void SelectionSort(int list[],int size)
 	for(pass=1;pass<size;pass++)
 	{
 		maxindex = FindMaxIndex(list,size-pass);
-		Exchange(&list[maxindex] ,&list[size-pass]);
+		if(list[maxindex]!=list[size-pass])
+		{
+			Exchange(&list[maxindex] ,&list[size-pass]);
+		}
 	}
 	return;
 }
