3c0f2e0d000c6670e34eb15ad298b0a4d7b60949
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-08-24 14:39:38 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -31,6 +31,68 @@ BSTNode * insert(BSTNode * root,int elem)
 		root -> rsTree = insert(root -> rsTree,elem);
 	return root;
 }
+BSTNode * FindMin(BSTNode * root)
+{
+	BSTNode * cursor;
+	if(root == NULL)
+		return NULL;
+	cursor = root;
+	while(NULL != cursor -> lsTree)
+	{
+		cursor = cursor -> lsTree;	
+	}
+	return cursor;
+}
+BSTNode * delete(BSTNode * root,int elem)
+{
+	BSTNode * pDel;
+	BSTNode * minrs;
+	//if root is NULL
+	if(root == NULL)
+		return NULL;
+	//if elem is root
+	if(elem == root -> data)
+	{
+		//root may be leaf
+		if(root->lsTree == NULL && root->rsTree == NULL)
+		{
+			pDel = root;
+			root = NULL;
+			free(pDel);
+			return root;
+		}
+		//root have one child left sub tree
+		else if(root->lsTree != NULL && root->rsTree == NULL)
+		{
+			pDel = root;
+			root = root->lsTree;
+			free(pDel);
+			return root;
+		}
+		//root have one child right sub tree
+		else if(root->lsTree == NULL && root->rsTree != NULL)
+		{
+			pDel = root;
+			root = root -> rsTree;
+			free(pDel);
+			return root;
+		} 
+		//root have two childs
+		else if(root->lsTree != NULL && root->rsTree != NULL)
+		{	
+			pDel = root;
+			minrs = FindMin(root -> rsTree);
+			root -> data = minrs -> data;
+			root -> rsTree = delete(root->rsTree,minrs->data);
+			return root;
+		}
+	}
+	if(elem < root -> data)
+		root->lsTree = delete(root->lsTree,elem);
+	if(elem > root -> data)
+		root->rsTree = delete(root->rsTree,elem);
+	return root;
+}
 void inOrder(BSTNode * root)
 {
 	if(NULL!=root)
@@ -68,13 +130,19 @@ int main(void)
 	root = insert(root,10);
 	root = insert(root,5);
 	inOrder(root);
+	
+	int delElem;
+	printf("Enter element to delete: ");
+	scanf("%d", &delElem);
+	root = delete(root, delElem);
+	inOrder(root);
 	printf("\nEnter a element to search : ");
 	scanf("%d",&key);
 	BSTNode * res;
 	res = Search(root,key);
 	if(NULL!=res)
 		printf("\nKey found\n");
-	else
+	else	
 		printf("\nKey not found\n");
 	return 0;
 }
